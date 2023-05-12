52daa01229d3521e07b9cd0ec71401ea8dcae6f9
AYUSHKUMAR <ayush763386@gmail.com>
2023-05-12 18:47:38 +0530

    ayush

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import Ak from "./ak";
 import Philips from "./ayush/Philips";
 function App() {
   return (
     <div>
-      <Ak></Ak>
+      <Philips></Philips>
     </div>
   );
 }
---
+++ src/coment/login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./termes.module.css";
+function Login({}) {
+  console.log(Login);
+  return <div className={styles["box"]}></div>;
+}
+
+export default Login;
