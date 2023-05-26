1bb1ccdf7ffd14d41cea275c8e2295998a65c958
AYUSHKUMAR <ayush763386@gmail.com>
2023-05-26 19:19:37 +0530

    cleck

--- src/cleck/show.js
+++ src/cleck/show.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./container.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSnowflake } from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown, faSnowflake } from "@fortawesome/free-solid-svg-icons";
 function Show() {
   return (
     <div className={styles["black"]}>
@@ -32,12 +32,188 @@ function Show() {
       </div>
       <div className={styles["chackbox"]}>
         <input type="checkbox" className={styles["box"]} />
-        <span>2/2 ITEMS SELECTED</span>
-        <p>REMOVE</p>
-        <p>MOVE TO WISHLIST</p>
+        <span className={styles["chack"]}>2/2 ITEMS SELECT</span>
+        <p className={styles["price"]}>REMOVE</p>
+        <div className={styles["are"]}></div>
+        <p className={styles["const"]}>MOVE TO WISHLIST</p>
+      </div>
+      <div className={styles["and"]}>
+        <ul>
+          <li>
+            <span className={styles["close"]}>&times;</span>
+          </li>
+        </ul>
+        <div className={styles["live"]}>
+          <input type="checkbox" className={styles["live"]} />
+        </div>
+        <img src="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/19851008/2022/11/3/2a497631-607e-4e6c-8bbd-2689345811d81667468790432-DREAM-WEAVERZ-Black-Leather-Covered-Stainless-Steel-Paper-Bi-1.jpg"></img>
+        <div className={styles["good"]}>
+          <p className={styles["dream"]}>DREAM WEAVERZ</p>
+          <p className={styles["paper"]}>
+            Black Leather Covered Stainless Steel Paper Bin
+          </p>
+          <p className={styles["sold"]}>Sold by:Dream Weaverz</p>
+
+          <div className={styles["size"]}>
+            <div className={styles["last"]}>
+              <button className={styles["men"]}>
+                Size:Onesize
+                <FontAwesomeIcon icon={faCaretDown} />
+              </button>
+            </div>
+            <div className={styles["fast"]}>
+              <button className={styles["work"]}>
+                Qty:1
+                <FontAwesomeIcon icon={faCaretDown} />
+              </button>
+            </div>
+          </div>
+          <div className={styles["game"]}>
+            <div className={styles["set"]}>
+              <p>₹749</p>
+            </div>
+            <div className={styles["number"]}>
+              <span className={styles["mad"]}>
+                <del>₹1,499</del>
+              </span>
+              &nbsp;&nbsp;
+              <span className={styles["land"]}>50% OFF</span>
+            </div>
+          </div>
+          <div className={styles["small"]}>
+            <div className={styles["return"]}>
+              <span>14 days</span>
+            </div>
+            &nbsp;
+            <p className={styles["space"]}>return available</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles["am"]}>
+        <ul>
+          <li>
+            <span className={styles["colose"]}>&times;</span>
+          </li>
+        </ul>
+        <div className={styles["livemis"]}>
+          <input type="checkbox" className={styles["livemis"]} />
+        </div>
+        <img src="	https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16071114/2022/12/11/0b3c8b51-c937-4b0f-b3d9-b28c869c3f1a1670740583113WLongWearPinkLipstick-PinkSync1.jpg"></img>
+        <div className={styles["good"]}>
+          <p className={styles["dream"]}>W</p>
+          <p className={styles["paper"]}>
+            Black Leather Covered Stainless Steel Paper Bin
+          </p>
+          <p className={styles["sold"]}>Long Wear Pink Lipstick - Pink Sync</p>
+
+          <div className={styles["size"]}>
+            <div className={styles["last"]}>
+              <button className={styles["men"]}>
+                Size:Onesize
+                <FontAwesomeIcon icon={faCaretDown} />
+              </button>
+            </div>
+            <div className={styles["fast"]}>
+              <button className={styles["work"]}>
+                Qty:1
+                <FontAwesomeIcon icon={faCaretDown} />
+              </button>
+            </div>
+          </div>
+          <div className={styles["game"]}>
+            <div className={styles["set"]}>
+              <p>₹519</p>
+            </div>
+            <div className={styles["number"]}>
+              <span className={styles["mad"]}>
+                <del>₹649</del>
+              </span>
+              &nbsp;&nbsp;
+              <span className={styles["land"]}>20% OFF</span>
+            </div>
+          </div>
+          <div className={styles["small"]}>
+            <div className={styles["return"]}>
+              <span>Exchange Only</span>
+            </div>
+            &nbsp;
+            <p className={styles["space"]}>return available</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles["number0"]}>
+        <div className={styles["select"]}>
+          <p>Select Quantity</p>
+        </div>
+        <div className={styles["number1"]}>
+          <input
+            type="text"
+            className={styles["red"]}
+            value="1"
+            disabled
+          ></input>
+          <input
+            type="text"
+            className={styles["red"]}
+            value="2"
+            disabled
+          ></input>
+          <input
+            type="text"
+            className={styles["red"]}
+            value="3"
+            disabled
+          ></input>
+          <input
+            type="text"
+            className={styles["red"]}
+            value="4"
+            disabled
+          ></input>
+          <input
+            type="text"
+            className={styles["red"]}
+            value="5"
+            disabled
+          ></input>
+          <div className={styles["number1"]}>
+            <input
+              type="text"
+              className={styles["red"]}
+              value="6"
+              disabled
+            ></input>
+            <input
+              type="text"
+              className={styles["red"]}
+              value="7"
+              disabled
+            ></input>
+            <input
+              type="text"
+              className={styles["red"]}
+              value="8"
+              disabled
+            ></input>
+            <input
+              type="text"
+              className={styles["red"]}
+              value="9"
+              disabled
+            ></input>
+            <input
+              type="text"
+              className={styles["red"]}
+              value="10"
+              disabled
+            ></input>
+          </div>
+        </div>
+        <button className={styles["button"]}>
+          <p>DONE</p>
+        </button>
       </div>
     </div>
   );
 }
-
 export default Show;
