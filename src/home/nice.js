ea9b3d3a7e76c13cd26b7b50c402e32ab0fc054a
AYUSHKUMAR <ayush763386@gmail.com>
2023-06-27 16:40:41 +0530

    update

--- src/home/nice.js
+++ src/home/nice.js
@@ -1,7 +1,7 @@
 import styles from "./nice.module.css";
 export default function nice() {
   return (
-    <div>
+    <div className={styles["black"]}>
       <div className={styles["furnish"]}>
         <img src="https://www.tavolisedie.com/modules/ph_simpleblog/featured/173.jpg"></img>
         <div className={styles["furnish1"]}>
@@ -14,23 +14,168 @@ export default function nice() {
       <div className={styles["see"]}>
         <h4>NICE TO SEE YOU, COME ON IN!</h4>
       </div>
-      <div className={styles["bed"]}>
-        <img src="https://m.media-amazon.com/images/I/51XFMwDcEYL._AC_UF894,1000_QL80_.jpg"></img>
-        <h4>Bed Lines</h4>
-        <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/04/A3848-How-to-enhance-Soft-Furnishing-Aesthetics-in-your-house.jpg"></img>
-        <h4>Furnishings</h4>
-      </div>
-      <div className={styles["bed"]}>
-        <img src="https://www.thespruce.com/thmb/WS_-YTuVWrZjUai8yT4c1CVo51E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/decorating-in-art-deco-style-1976535-03-6a40998a14ef49e38cbbe0ca4ed7cdf9.jpg"></img>
-        <h4>Art & Decor</h4>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2O71-hPONYehFljIc5aKQxpcB4dSmR8iSh87o6pUbopFlSHw6FNgQuJOfr4aDz6X5n04&usqp=CAU"></img>
-        <h4>Lamps & Lighting</h4>
-      </div>
-      <div className={styles["bed"]}>
-        <img src="https://www.thespruce.com/thmb/xIzyfrp-eWD3aCt5_l4r08iS3JM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/small-dining-room-ideas-5194506-hero-4925b02521e14904893178839e9a3ea9.jpg"></img>
-        <h4>Art & Decor</h4>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvOklS8aXdbIZGdR32G96TqVRInGvs5Gudg&usqp=CAU"></img>
-        <h4>Lamps & Lighting</h4>
+      <div className={styles["bed1"]}>
+        <div className={styles["bed"]}>
+          <img src="https://assets.myntassets.com/f_webp,w_350,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2018/2/2/11517563660020-bed.jpg"></img>
+          <img
+            className={styles["src"]}
+            src="https://assets.myntassets.com/f_webp,w_350,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2018/2/2/11517563782565-furnishings.jpg"
+          ></img>
+          <img src="https://assets.myntassets.com/f_webp,w_350,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2018/2/2/11517563810789-art-n-decor.jpg"></img>
+          <img
+            className={styles["src1"]}
+            src="https://assets.myntassets.com/f_webp,w_350,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2018/2/2/11517563866590-lamps.jpg"
+          ></img>
+          <img src="https://assets.myntassets.com/f_webp,w_350,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2018/2/2/11517563925069-kitchen.jpg"></img>
+          <img
+            className={styles["src1"]}
+            src="https://assets.myntassets.com/f_webp,w_350,c_limit,fl_progressive,dpr_2.0/assets/images/banners/2018/2/2/11517563958962-bath.jpg"
+          ></img>
+          {/* <div className={styles["hover01 column"]}>
+            <div className={styles["figure"]}>
+              <img src="https://picsum.photos/300/200?image=244" />
+            </div>
+          </div> */}
+        </div>
+        <div className={styles["lines"]}>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Furnishings & Bed Linen</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Bed Sheets</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Blankets, Quilts & Dohars</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Pillows & Pillow Covers</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Cushions & Cushion Covers</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Curtains</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Bath & Bed Linen</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Towels & Towel Sets</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Bath Robes</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Bathroom Accessories</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Shower Curtains</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Bath Rugs</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Home Décor</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Pooja Essentials</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Laptop Table</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Ottomans</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Artificial Flowers & Plants</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Candles & Candle Holders</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Clocks</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Planters & Garden Accessories</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Home Fragrances</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Mirrors</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Wall Art</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Wall Décor</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Wall Shelves</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Showpieces & Vases</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Lamps & Lighting</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Floor Lamps</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Table Lamps</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Wall Lamp & String Lights</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>
+              Bathroom Lights & Outdoor Lamps
+            </a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Ceiling Lamps</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Kitchen & Table</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Dinnerware</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Cups & Mugs</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Water Bottles</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Bakeware</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Trays & Serveware</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Bar & Drinkware</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Cookware & Kitchen Tools</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Kitchen Storage</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Table Covers & Furnishings</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Storage & Organisers</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Organisers</a>
+          </div>
+          <div className={styles["column-base"]}>
+            <a className={styles["navi-link"]}>Hooks & Holders</a>
+          </div>
+        </div>
       </div>
       <div className={styles["set"]}>
         <h1>Set Inspired..</h1>
