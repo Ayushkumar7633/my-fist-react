df125b73cb84aa5e9c50c921099a8a94c29962c2
AYUSHKUMAR <ayush763386@gmail.com>
2023-07-14 12:47:49 +0530

    update_fast

--- src/fast1/fast.jsx
+++ src/fast1/fast.jsx
@@ -3,40 +3,45 @@ export default function Fast() {
   return (
     <div className={styles["black"]}>
       <h2 className={styles["header"]}>
-        Whether you’re a startup or an established business, here’s why Dukaan
-        is your best choice
+        Whether you're a fledgling venture or a well-established enterprise,
+        here's why Dukaan is the ultimate option.
       </h2>
       <div className={styles["container1"]}>
         <div className={styles["image"]}>
           <img
-            src="https://mydukaan.io/_next/static/images/launch-fast-6349265d2de0cf16e61e160f85bd681d.svg"
+            src="https://media.sproutsocial.com/uploads/2020/02/landing-page-examples.jpg"
             alt=""
           />
         </div>
         <div className={styles["container"]}>
-          <h2>Launch Fast</h2>
-          <p>Fully responsive e-commerce website & mobile app.</p>
-          <p>Loads 6X faster than existing solutions.</p>
-          <p>Upload/import products and inventory in bulk.</p>
-          <p>Integrate payment gateways.</p>
-          <p>Easily customizable themes.</p>
+          <h2>speedy rollout.</h2>
+          <p>adaptive e-commerce platform and mobile application.</p>
+          <p>
+            provides twice the loading speed compared to current alternatives.
+          </p>
+          <p>bulk product and inventory upload/import.</p>
+          <p>implement payment gateways.</p>
+          <p>flexible and modifiable templates.</p>
         </div>
       </div>
       <div className={styles["content1"]}>
         <div className={styles["ayush"]}>
-          <h2>Scale Faster</h2>
+          <h2>expedite scalability.</h2>
           <p>
-            Guaranteed 99.5% uptime for your store - We keep you open for
-            business.
+            ensured 95.9% operational time for your store - we maintain
+            uninterrupted availability.
           </p>
-          <p>60+ third party plugins.</p>
-          <p>Marketing tools and discounts to drive repeat orders.</p>
-          <p>Add staff accounts, assign different roles.</p>
-          <p>Unlimited transactions, 0% transaction fees.</p>
+          <p>an expansive collection of over 50 external plugins.</p>
+          <p>
+            promotional resources and incentives to encourage recurring
+            purchases.
+          </p>
+          <p>create team members' profiles with distinct responsibilities.</p>
+          <p>unrestricted transactions with no transaction charges.</p>
         </div>
-        <div className={styles["content"]}>
+        <div className={styles["image"]}>
           <img
-            src="https://mydukaan.io/_next/static/images/scale-faster-615089c61891323e0c2e2f0a4d498d53.svg"
+            src="https://whatisop.com/wp-content/uploads/2020/05/12-Tips-to-Design-Bespoke-Landing-Pages-That-CONVERTS.jpg"
             alt=""
           />
         </div>
@@ -44,17 +49,134 @@ export default function Fast() {
       <div className={styles["manage"]}>
         <div className={styles["image"]}>
           <img
-            src="https://mydukaan.io/_next/static/images/manage-better-5f03835fb0602b9fa6a4b3fc98bc9c7c.svg"
+            src="https://www.mindinventory.com/blog/wp-content/uploads/2022/10/landing-page-design1200-1024x512.png"
             alt=""
           />
         </div>
         <div className={styles["better"]}>
-          <h2>Launch Fast</h2>
+          <h2>enhance operational control.</h2>
           <p>Fully responsive e-commerce website & mobile app.</p>
           <p>Loads 6X faster than existing solutions.</p>
           <p>Upload/import products and inventory in bulk.</p>
           <p>Integrate payment gateways.</p>
           <p>Easily customizable themes.</p>
+        </div>
+      </div>
+      <div className={styles["commerce1"]}>
+        <div className={styles["commerce"]}>
+          <h2 className={styles["header-h2"]}>
+            Simplified Online Selling, Amplified Results.
+          </h2>
+          <p className={styles["header-p"]}>
+            Streamlined Online Sales, Maximized Performance.
+          </p>
+          <div className={styles["site"]}>
+            <div className={styles["home-toole"]}>
+              <div className={styles["banner-box"]}>
+                <img src="" alt="" />
+                <h3>page load time.</h3>
+                <p>
+                  Lightning-fast online shops. Put it to the test and begin
+                  selling online to witness it firsthand!
+                </p>
+              </div>
+              <div className={styles["banner-box1"]}>
+                <img src="" alt="" />
+                <h3>Multi-Depot.</h3>
+                <p>
+                  Unified store, distributed inventory. Fulfill orders from
+                  multiple warehouses spanning across India.
+                </p>
+              </div>
+              <div className={styles["banner-box2"]}>
+                <img src="" alt="" />
+
+                <h3>enhanced checkout experiences.</h3>
+                <p>
+                  provide a frictionless shopping journey tailored for smooth
+                  checkouts and decrease abandonment rates.
+                </p>
+              </div>
+              <div className={styles["banner-box3"]}>
+                <img src="" alt="" />
+                <h3>team member accounts.</h3>
+                <p>
+                  invite and onboard team members, collaborators, and partners
+                  to support business growth while controlling permissions.
+                </p>
+              </div>
+              <div className={styles["banner-box4"]}>
+                <img src="" alt="" />
+                <h3>application for Android devices.</h3>
+                <p>
+                  Embrace the mobile era. Make your store mobile-friendly.
+                  Attract and retain loyal customers with your dedicated mobile
+                  application.
+                </p>
+              </div>
+              <div className={styles["banner-box5"]}>
+                <img src="" alt="" />
+                <h3>advanced data insights.</h3>
+                <p>
+                  Instant access to comprehensive data on your sales, website
+                  traffic, geographic distribution, and product insights with a
+                  single click.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles["home-thems1"]}>
+        <div className={styles["home-thems"]}>
+          <h2 className={styles["header-h"]}>
+            Get your online store up and running with these themes
+          </h2>
+          <div className={styles["inner"]}>
+            <div className={styles["inner1"]}>
+              <img
+                src="	https://mydukaan.io/_next/static/images/tinker-0b59397a813a30dccbf17d9278372824.webp"
+                alt=""
+              />
+              <h3>Tinker</h3>
+            </div>
+            <div className={styles["inner1"]}>
+              <img
+                src="	https://mydukaan.io/_next/static/images/ursa-047137008aa0cf119dc1a54047b7fd87.png"
+                alt=""
+              />
+              <h3>Ursa</h3>
+            </div>
+            <div className={styles["inner1"]}>
+              <img
+                src="https://mydukaan.io/_next/static/images/enigma-b98c9aff293be1a8103e3f5bf15682e7.png"
+                alt=""
+              />
+              <h3>Enigma</h3>
+            </div>
+            <div className={styles["inner1"]}>
+              <img
+                src="	https://mydukaan.io/_next/static/images/mana-3d58c03ccebc3fb7ccd5e1035aca044c.webp"
+                alt=""
+              />
+              <h3>Mana</h3>
+            </div>
+            <div className={styles["inner1"]}>
+              <img
+                src="https://mydukaan.io/_next/static/images/nirvana-0d20aed96a17d45b4de7df85875d5731.webp"
+                alt=""
+              />
+              <h3>Nirvana</h3>
+            </div>
+            <div className={styles["inner1"]}>
+              <img
+                src="https://mydukaan.io/_next/static/images/nirvana-0d20aed96a17d45b4de7df85875d5731.webp"
+                alt=""
+              />
+              <h3>Oxford</h3>
+            </div>
+          </div>
+          <div className={styles["btn"]}></div>
         </div>
       </div>
     </div>
